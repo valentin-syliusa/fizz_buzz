cb663b166971ba1d09a08bbf90e97e803f1f752a
Valentin <valentin.aranega@syliusa.net>
2022-03-06 14:00:16 +0100

    UI improvements

--- app/src/main/java/com/aranegav/fizzbuzz/presentation/result/list/ResultRecyclerViewHolder.kt
+++ app/src/main/java/com/aranegav/fizzbuzz/presentation/result/list/ResultRecyclerViewHolder.kt
@@ -5,6 +5,6 @@ import androidx.recyclerview.widget.RecyclerView
 class ResultRecyclerViewHolder(private val resultView: ResultView) :
     RecyclerView.ViewHolder(resultView) {
         fun bind(result: String) {
-
+            resultView.result = result
         }
 }
