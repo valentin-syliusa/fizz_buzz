502bf0e7bba3fbf06e024acc83dd5980ada2f243
Valentin <valentin.aranega@syliusa.net>
2022-03-06 15:05:45 +0100

    Implemented Domain + Hilt + Input logic/ViewModel

--- app/src/main/java/com/aranegav/fizzbuzz/presentation/input/InputFragment.kt
+++ app/src/main/java/com/aranegav/fizzbuzz/presentation/input/InputFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.aranegav.fizzbuzz.databinding.InputLayoutBinding
 
 class InputFragment : Fragment() {
@@ -13,6 +14,8 @@ class InputFragment : Fragment() {
 
     private val binding: InputLayoutBinding
         get() = _binding!!
+
+    private val viewModel: InputViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +29,29 @@ class InputFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.observableState.observe(viewLifecycleOwner) {
+            it?.let { state ->
+                when (state) {
+                    is State.InvalidInput -> {
+                        binding.int1Valueinputview.stateText =
+                            if (state.isInt1Valid) "Donnée valide" else "Donnée invalide"
+                        binding.int2Valueinputview.stateText =
+                            if (state.isInt2Valid) "Donnée valide" else "Donnée invalide"
+                        binding.limitValueinputview.stateText =
+                            if (state.isLimitValid) "Donnée valide" else "Donnée invalide"
+                        binding.str1Valueinputview.stateText =
+                            if (state.isStr1Valid) "Donnée valide" else "Donnée invalide"
+                        binding.str2Valueinputview.stateText =
+                            if (state.isStr2Valid) "Donnée valide" else "Donnée invalide"
+                        binding.validateButton.isEnabled = false
+                    }
+                    is State.ValidInput -> {
+                        binding.validateButton.isEnabled = true
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/aranegav/fizzbuzz/presentation/input/InputViewModel.kt
@@ -0,0 +1,91 @@
+package com.aranegav.fizzbuzz.presentation.input
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+data class InputData(
+    val int1: Int? = null,
+    val int2: Int? = null,
+    val limit: Int? = null,
+    val str1: String? = null,
+    val str2: String? = null,
+)
+
+sealed class State {
+    data class InvalidInput(
+        val isInt1Valid: Boolean,
+        val isInt2Valid: Boolean,
+        val isLimitValid: Boolean,
+        val isStr1Valid: Boolean,
+        val isStr2Valid: Boolean,
+    ) : State()
+
+    object ValidInput : State()
+}
+
+@HiltViewModel
+class InputViewModel @Inject constructor(): ViewModel() {
+
+    private val state = MutableLiveData<State>()
+
+    val observableState: LiveData<State> = state
+
+    private var inputData = InputData()
+
+    fun updateInt1Input(inputAsString: String) {
+        inputData = inputData.copy(
+            int1 = inputAsString.toIntOrNull()
+        )
+        updateState()
+    }
+
+    fun updateInt2Input(inputAsString: String) {
+        inputData = inputData.copy(
+            int2 = inputAsString.toIntOrNull()
+        )
+        updateState()
+    }
+
+    fun updateLimitInput(inputAsString: String) {
+        inputData = inputData.copy(
+            limit = inputAsString.toIntOrNull()
+        )
+        updateState()
+    }
+
+    fun updateStr1Input(inputAsString: String) {
+        inputData = inputData.copy(
+            str1 = inputAsString
+        )
+        updateState()
+    }
+
+    fun updateStr2Input(inputAsString: String) {
+        inputData = inputData.copy(
+            str2 = inputAsString
+        )
+        updateState()
+    }
+
+    private fun updateState() {
+        state.value = if (inputData.int1 != null &&
+            inputData.int2 != null &&
+            inputData.limit != null &&
+            inputData.str1 != null &&
+            inputData.str2 != null
+        ) {
+            State.ValidInput
+        } else {
+            State.InvalidInput(
+                isInt1Valid = inputData.int1 != null,
+                isInt2Valid = inputData.int2 != null,
+                isLimitValid = inputData.limit != null,
+                isStr1Valid = inputData.str1 != null,
+                isStr2Valid = inputData.str2 != null
+            )
+        }
+    }
+}
